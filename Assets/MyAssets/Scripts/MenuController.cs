570de525f5a35c5885f2aff7182f5611b2db91b8
Jackson Doherty <jgdoherty23@gmail.com>
2016-12-05 23:28:36 -0600

    initial commit

---
+++ Assets/MyAssets/Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject itemSpawnLocation;
+    public GameObject gun;
+    public GameObject bow;
+    public GameObject arrow;
+    public GameObject lightBox;
+
+    public void GunButtonListener() {
+        Instantiate(gun, itemSpawnLocation.transform.position, itemSpawnLocation.transform.rotation);
+    }
+
+    public void BowAndArrowButtonListener() {
+        Instantiate(bow, itemSpawnLocation.transform.position, itemSpawnLocation.transform.rotation);
+        for (int i = 1; i <= 5; i++) {
+            Instantiate(arrow, itemSpawnLocation.transform.position, itemSpawnLocation.transform.rotation);
+        }
+    }
+
+    public void LightBoxButtonListener() {
+        Instantiate(lightBox, itemSpawnLocation.transform.position, itemSpawnLocation.transform.rotation);
+    }
+}
---
+++ Assets/MyAssets/Scripts/MenuOpener.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace VRTK {
+    public class MenuOpener : MonoBehaviour {
+
+        public GameObject menu;
+
+        // Use this for initialization
+        void Start() {
+            menu.SetActive(false);
+            GetComponent<VRTK_ControllerEvents>().ApplicationMenuPressed += new ControllerInteractionEventHandler(OpenCloseMenu);
+        }
+
+        // Update is called once per frame
+        void Update() {
+        }
+
+        private void OpenCloseMenu(object sender, ControllerInteractionEventArgs e) {
+            if (menu.activeSelf) {
+                menu.SetActive(false);
+            } else {
+                menu.SetActive(true);
+            }
+        }
+    }
+}
